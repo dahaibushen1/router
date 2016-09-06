e0c977e93023518320ccd1fc6a7eeffef0cf8222
dahaibushen1 <991779153@qq.com>
2016-09-06 22:10:43 +0800

    自定义控制器

---
+++ RouterPush/RouterPush/CustomController.h
@@ -0,0 +1,13 @@
+//
+//  CustomController.h
+//  RouterPush
+//
+//  Created by hu on 16/9/6.
+//  Copyright © 2016年 hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomController : UIViewController
+
+@end
---
+++ RouterPush/RouterPush/CustomController.m
@@ -0,0 +1,13 @@
+//
+//  CustomController.m
+//  RouterPush
+//
+//  Created by hu on 16/9/6.
+//  Copyright © 2016年 hu. All rights reserved.
+//
+
+#import "CustomController.h"
+
+@implementation CustomController
+
+@end
