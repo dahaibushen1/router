bbf5d796096fa7473222a49331638d788101f5ab
dahaibushen1 <991779153@qq.com>
2016-09-06 22:12:22 +0800

    创建详情控制器

---
+++ RouterPush/RouterPush/DetailController.h
@@ -0,0 +1,13 @@
+//
+//  DetailController.h
+//  RouterPush
+//
+//  Created by hu on 16/9/6.
+//  Copyright © 2016年 hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailController : UIViewController
+
+@end
---
+++ RouterPush/RouterPush/DetailController.m
@@ -0,0 +1,14 @@
+
+//
+//  DetailController.m
+//  RouterPush
+//
+//  Created by hu on 16/9/6.
+//  Copyright © 2016年 hu. All rights reserved.
+//
+
+#import "DetailController.h"
+
+@implementation DetailController
+
+@end
