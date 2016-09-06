9f111c0a9152b3b913eb2b37eff9e7b9ccd2a37a
dahaibushen1 <991779153@qq.com>
2016-09-06 22:15:36 +0800

    控制器命名

--- RouterPush/RouterPush/ViewController.m
+++ RouterPush/RouterPush/ViewController.m
@@ -16,7 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+   
+    self.title = @"基本控制器";
+    
 }
 
 - (void)didReceiveMemoryWarning {
