732734c5fb76d12e7a8939ea81db8913f87e1b2f
dahaibushen1 <991779153@qq.com>
2016-09-22 22:34:33 +0800

    主分支

--- RouterPush/RouterPush/ViewController.m
+++ RouterPush/RouterPush/ViewController.m
@@ -22,7 +22,7 @@
     UILabel * label = [[UILabel alloc] init];
     label.frame = CGRectMake(30, 300, 300, 30);
     label.backgroundColor = [UIColor whiteColor];
-    label.text = @"这里是master.....";
+    label.text = @"master 主分支使用这个";
     [self.view addSubview:label];
 
 }
